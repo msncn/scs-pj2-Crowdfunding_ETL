e13175b115724fdb5755df2cf3051ea9327cad12
msncn <shengnancn.ma@mail.utoronto.ca>
2023-07-24 20:25:33 -0400

    final

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,41 @@
+-- Create category table
+CREATE TABLE category (
+	category_id CHAR(4) PRIMARY KEY NOT NULL,
+	category VARCHAR(50) NOT NULL
+	);
+
+-- Create subcategory table
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	subcategory VARCHAR(50) NOT NULL
+	);
+
+-- Create contacts table
+CREATE TABLE contacts (
+	contact_id INT PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL
+	);
+
+-- Create campaign table
+CREATE TABLE campaign (
+	cf_id INT PRIMARY KEY NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR(50) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	goal numeric(10,2) NOT NULL,
+	pledged numeric(10,2) NOT NULL,
+	outcome VARCHAR(10) NOT NULL,
+	backers_count INT NOT NULL,
+	country CHAR(2) NOT NULL,
+	currency CHAR(3) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id CHAR(4) NOT NULL,
+	subcategory_id VARCHAR(10) NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+	);
+
